da0da095f0d8b6c7f648a714083046124e00e0c8
Skava600 <56450477+Skava600@users.noreply.github.com>
2020-05-17 15:34:55 +0300

    Добавлена 3 и 5 лаба

---
+++ lab3,5/Army.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab3
+{
+    //*****Перечисления*************************************************
+    enum Type
+    {
+        HorseMan = 1,
+        Infantry,
+        Archer,
+        HorseArcher
+    }
+    enum RangeUnits
+    {
+        HorseMan = 0,
+        Infantry = HorseMan,
+        HorseArcher = 100,
+        Archer = HorseArcher
+    }
+    //***********************************************************
+
+    //******ИНДЕКСАТОР*********************************************
+    class Army
+    {
+        protected Unit[] army;
+        public static int size;
+        protected int MAXSIZE = 20;
+        public Army()
+        {
+            army = new Unit[MAXSIZE];
+            size = 0;
+        }
+        public Unit this[int index]
+        {
+            get
+            {
+                return army[index];
+            }
+            set
+            {
+                if (size == MAXSIZE)
+                {
+                    Console.WriteLine("Достигнут лимит");
+                    return;
+                }
+
+                size++;
+                army[index] = value;
+            }
+        }
+        public void ShowArmyTypes()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"{i}.");
+                this[i].ShowType();
+            }
+            Console.WriteLine("1 - Всадник\n2 - Пехота\n3 - Лучник \n4 - Конный лучник");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(this[i].GetIntType());
+            }
+        }
+
+
+
+
+    };
+    //***********************************************************8
+}
---
+++ lab3,5/HorseMan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace lab3
+{
+    class HorseMan : Infantry, IHorse
+    {
+       
+        double IHorse.Speed { get => speedUnit; set => speedUnit = value; }
+        public HorseMan() : base(15, 4, 0.6, 0, "Кавалерия") 
+        { 
+            speedUnit = 10; 
+            rangeUnit = (int)RangeUnits.HorseMan;
+            intType = Type.HorseMan;
+        }
+        public override void Train()
+        {
+            Console.WriteLine($"{type} проскакал по тренировочному полю и ");
+            base.Train();
+            if (speedUnit < IHorse.MAXSPEED)
+                speedUnit++;
+            else Console.WriteLine("Вы не можете выжать из лошади большую скорость.");
+        }
+
+
+    };
+
+    //*************************************************************
+}
