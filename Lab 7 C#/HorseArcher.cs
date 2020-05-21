9a918ed5bde4624c483440380e3cc4ce188b24d8
Skava600 <56450477+Skava600@users.noreply.github.com>
2020-05-21 15:08:58 +0300

    Added 8 lab(check this instead 3,5,6 labs)

---
+++ Lab 7 C#/HorseArcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab3
+{
+    class HorseArcher : Archer, IHorse
+    {
+        float IHorse.Speed { get => SpeedUnit; set => SpeedUnit = value; }
+        public override void Train()
+        {
+            Console.WriteLine($"{TypeStr} galloped across the train field and  ");
+            if(SpeedUnit < IHorse.MAXSPEED)
+                 SpeedUnit++;
+            base.Train();
+        }
+        public HorseArcher() : base(rnd.Next(10,25), rnd.Next(3,MAXATTACK), 0.6f, 0, "Horse Archer") // hp,damage,armour,speed,type
+        {
+            SpeedUnit = rnd.Next(8, IHorse.MAXSPEED);
+            RangeUnit = RangeUnit.HorseArcher;
+            IntType = Type.HorseArcher;
+        }
+    }
+}
+
