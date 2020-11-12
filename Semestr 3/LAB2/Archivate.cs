de492d76d183d98e52c2b969dbb8015c14adee70
Skava600 <56450477+Skava600@users.noreply.github.com>
2020-11-12 19:30:23 +0300

    Add files via upload

---
+++ Semestr 3/LAB2/Archivate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+
+namespace LAB2
+{
+    class Archivate
+    {
+        public static void Compress(string source, string target)
+        {
+            using (FileStream ss = new FileStream(source, FileMode.Open))
+            {
+                using (FileStream ts = File.Create(target))
+                {
+                    using (GZipStream zs = new GZipStream(ts, CompressionLevel.Optimal))
+                    {
+                        ss.CopyTo(zs);
+                    }
+                }
+
+            }
+        }
+
+        public static void Decompress(string source, string target)
+        {
+            using (FileStream ss = new FileStream(source, FileMode.OpenOrCreate))
+            {
+                using (FileStream ts = File.Create(target))
+                {
+                    using (GZipStream zs = new GZipStream(ss, CompressionMode.Decompress))
+                    {
+                        zs.CopyTo(ts);
+                    }
+                }
+            }
+        }
+    }
+}
