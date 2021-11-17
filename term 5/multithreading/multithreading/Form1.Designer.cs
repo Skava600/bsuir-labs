2ac8d0b98e20296f58dd65dc739399804a15d357
Skava600 <uskava7@gmail.com>
2021-11-17 18:54:30 +0300

    complete: 5t-system-programming-lab_3.

---
+++ term 5/multithreading/multithreading/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace multithreading
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.threadCountBox = new System.Windows.Forms.TextBox();
+            this.labelSorted = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.textBox1.Font = new System.Drawing.Font("Mongolian Baiti", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(194, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(393, 34);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.BackColor = System.Drawing.Color.RoyalBlue;
+            this.buttonSort.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSort.Location = new System.Drawing.Point(311, 155);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(136, 43);
+            this.buttonSort.TabIndex = 1;
+            this.buttonSort.Text = "Sort";
+            this.buttonSort.UseVisualStyleBackColor = false;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // threadCountBox
+            // 
+            this.threadCountBox.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.threadCountBox.Font = new System.Drawing.Font("Mongolian Baiti", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.threadCountBox.Location = new System.Drawing.Point(84, 68);
+            this.threadCountBox.Name = "threadCountBox";
+            this.threadCountBox.Size = new System.Drawing.Size(47, 34);
+            this.threadCountBox.TabIndex = 3;
+            // 
+            // labelSorted
+            // 
+            this.labelSorted.AutoSize = true;
+            this.labelSorted.Font = new System.Drawing.Font("Mongolian Baiti", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSorted.Location = new System.Drawing.Point(215, 240);
+            this.labelSorted.Name = "labelSorted";
+            this.labelSorted.Size = new System.Drawing.Size(0, 24);
+            this.labelSorted.TabIndex = 2;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Mongolian Baiti", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(215, 291);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(0, 24);
+            this.timeLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.threadCountBox);
+            this.Controls.Add(this.labelSorted);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.TextBox threadCountBox;
+        private System.Windows.Forms.Label labelSorted;
+        private System.Windows.Forms.Label timeLabel;
+    }
+}
+
---
+++ term 5/multithreading/multithreading/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multithreading
+{
+    public partial class Form1 : Form
+    {
+        private int[] valueArray;
+        private Hashtable threadHolder = new Hashtable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            threadHolder.Clear();
+            valueArray = textBox1.Text.Split().Select(n => Convert.ToInt32(n)).ToArray();
+
+            long startTime = DateTime.Now.Ticks;
+            QuickSort.ParallelSorting(valueArray, Convert.ToInt32(threadCountBox.Text));
+            long endTime = DateTime.Now.Ticks;
+            labelSorted.Text = String.Join("", new List<int>(valueArray).ConvertAll(i => i.ToString()).ToArray());
+            timeLabel.Text = $"Threads {threadCountBox.Text}, Elasped time: {Convert.ToString((endTime - startTime) / 10_000_000d)}";
+        }
+
+
+    }
+}
