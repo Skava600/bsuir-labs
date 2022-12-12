5c2a0b7c892fbf3b17aa03bcf6f0b3294895c7fa
Skava600 <uskava7@gmail.com>
2022-12-12 20:54:34 +0300

    7term-mathmod-4lab theoretical stats.

---
+++ term 7/MathMod/QueuingSystem/QueuingSystem/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace QueuingSystem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Canteen canteen = new Canteen();
+            canteen.X = 2;
+            canteen.p = 3.5;
+            canteen.q = 0.7;
+            canteen.t = 3;
+
+            double averageQueueLength = canteen.GetAverageQueueLength();
+            double averageStayTime = canteen.GetAverageStayTime();
+            if (canteen.isStationary)
+            {
+                Console.WriteLine($"Система стабильна и стационарна ");
+                Console.WriteLine($"средняя длина очереди {averageQueueLength}");
+                Console.WriteLine($"средняя длина пребывания в столовой {averageStayTime}");
+            }
+            else
+            {
+                Console.WriteLine("Система нестабильна, очередь бесконечна.");
+            }
+
+           
+        }
+    }
+}
